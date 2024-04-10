285362ddc34d8ad1fc627929b126b1117825d0c9
Dana Pugliano <dananpugliano@gmail.com>
2024-04-10 13:28:11 -0600

    Scoreboard Page Works

--- src/scoreboard/scoreboard.jsx
+++ src/scoreboard/scoreboard.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './scoreboard.css';
+
+
 export function Scoreboard() {
+    const [scores, setScores] = useState([]);
+
+    useEffect(() => {
+        async function loadScores() {
+            let currentUser = localStorage.getItem('userName');
+            let response = await fetch(`/api/scores/${currentUser}`);
+            const scoresData = await response.json();
+            setScores(scoresData);
+        }
+
+        loadScores();
+    }, []);
+
     return (
-        <main >
-            <div>scores displayed here</div>
+        <main>
+            <table className="table table-warning table-striped-columns">
+                <thead className="table-dark">
+                    <tr>
+                        <th>User</th>
+                        <th>$ Saved</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {scores.length > 0 ? (
+                        scores.map((score, index) => (
+                            <tr key={index}>
+                                <td>{score.username}</td>
+                                <td>{score.score}</td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan="2">Be the first to score</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </main>
     );
-};
+}
