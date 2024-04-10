aff160a198784164491f77285e03cc802f76eaca
Dana Pugliano <dananpugliano@gmail.com>
2024-04-10 10:38:50 -0600

    React Framework functioning

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import './app.css';
 import { Login } from './login/login';
 import { About } from './about/about';
 import { Scoreboard } from './scoreboard/scoreboard';
 import { Budget } from './budget/budget';
 
+
+
+function NotFound() {
+    return <main> 404. Return to sensitiveHeaders. Address unknown</main>;
+}
+
 export default function App() {
-    return <body>
+    return (
+        <BrowserRouter>
+    <body>
     <header>
         <h1>Budget Tracker</h1>
         <nav> 
             <menu>
-                <li><a href="index.html"> Home</a></li>
+                <li className='nav-item'>
+                    <NavLink className='nav-link' to=''>
+                    Login
+                    </NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink className='nav-link' to='budget'>
+                        Budget
+                    </NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink className='nav-link' to='scoreboard'>
+                        Scoreboard
+                    </NavLink>
+                </li>
+                <li className='nav-item'>
+                    <NavLink className='nav-link' to='about'>
+                        About
+                    </NavLink>
+                </li>
             </menu>
         </nav>
     </header>
 
 
-    <Login />
-    <About />
-    <Budget />
-    <Scoreboard />
+    <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/budget' element={<Budget />} />
+        <Route path='/scoreboard' element={<Scoreboard/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='*' element={<NotFound/>} />
+    </Routes>
+    
     
 
     <footer>
@@ -29,4 +61,6 @@ export default function App() {
         <a href = "https://github.com/danapug/startup"> GitHub</a>
     </footer>
 </body>
+</BrowserRouter>
+    );
 }
