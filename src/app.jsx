6c4e2f4996acc3b243127ad202095ecdb78bde8b
Dana Pugliano <dananpugliano@gmail.com>
2024-04-10 09:27:07 -0600

    React app functioning

---
+++ src/app.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+    return <body>
+    <header>
+        <h1>Budget Tracker</h1>
+        <nav> 
+            <menu>
+                <li><a href="index.html"> Home</a></li>
+            </menu>
+        </nav>
+    </header>
+
+
+    <main> 
+        <h1>Welcome</h1>
+            
+            
+    </main>
+    
+    
+
+    <footer>
+        <span class = "text-reset"> Author:</span>
+        <a href = "https://github.com/danapug/startup"> GitHub</a>
+    </footer>
+</body>
+}
